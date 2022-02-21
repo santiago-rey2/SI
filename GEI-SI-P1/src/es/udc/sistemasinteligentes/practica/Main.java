bee2f40104c95a2f6e1c45864647154cba8f9016
santiago-rey2 <santiago.rey2@udc.es>
2022-02-21 16:06:43 +0100

    primer commit

---
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/practica/Main.java
@@ -0,0 +1,18 @@
+package es.udc.sistemasinteligentes.practica;
+
+import es.udc.sistemasinteligentes.EstrategiaBusqueda;
+import es.udc.sistemasinteligentes.ProblemaBusqueda;
+import es.udc.sistemasinteligentes.ejemplo.Estrategia4;
+import es.udc.sistemasinteligentes.ejemplo.ProblemaAspiradora;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        ProblemaAspiradora.EstadoAspiradora estadoInicial = new ProblemaAspiradora.EstadoAspiradora(ProblemaAspiradora.EstadoAspiradora.PosicionRobot.IZQ,
+                                                                                                    ProblemaAspiradora.EstadoAspiradora.PosicionBasura.AMBAS);
+        ProblemaBusqueda aspiradora = new ProblemaAspiradora(estadoInicial);
+
+        EstrategiaBusqueda buscador = new Estrategia4();
+        System.out.println(buscador.soluciona(aspiradora));
+    }
+}
